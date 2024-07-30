e2e6ff61cef63d2a0c3cdd236855280eb7f464d5
Magno Gouveia <owlflee@gmail.com>
2024-07-30 11:43:35 -0300

    Add google conversion

--- js/main.js
+++ js/main.js
@@ -26,6 +26,23 @@
     });
 
 
+    $(".conversion").click(function(event) {
+        event.preventDefault(); // Evita o redirecionamento imediato
+        var urlDestino = event.currentTarget.href; // Pega a URL de destino do botão
+        console.log(`Conversion registered for url: ${urlDestino} `)
+        gtag('event', 'conversion', { 
+            'send_to': 'AW-16626198590/43b2CIul5r0ZEL7Q_vc9', 
+            'value': 1.0, 
+            'currency': 'BRL',
+            'event_destination_url': urlDestino
+        });
+    
+        setTimeout(function() {
+            window.location.href = urlDestino;
+        }, 500); // 500 ms de atraso para garantir que o evento seja enviado
+    })
+
+
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         center: true,
